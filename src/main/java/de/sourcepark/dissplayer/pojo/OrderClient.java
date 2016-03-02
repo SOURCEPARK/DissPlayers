d582a68b1d395db1ac4f1ef7d23ee9c536329419
Julius Naperkowski <jnaperkowski@sourcepark.de>
2016-03-02 15:23:54 +0100

    added convenience method for decreasing the number of slot items

--- src/main/java/de/sourcepark/dissplayer/pojo/OrderClient.java
+++ src/main/java/de/sourcepark/dissplayer/pojo/OrderClient.java
@@ -17,8 +17,9 @@ import java.util.Arrays;
  */
 public class OrderClient {
 
-    //REST URL
-    private static final String REST_URL = "http://localhost:9999/control/order/";
+    //Hubba Bubba REST URLs
+    private static final String REST_URL_ORDER_SERVICE = "http://localhost:9999/control/order/";
+    private static final String REST_URL_SLOT_DECREASE_ITEM_COUNT_SERVICE = "http://localhost:9999/control/slot/%d/items/decrease";
 
     /**
      *  Call order REST Service
@@ -28,7 +29,7 @@ public class OrderClient {
     public static String callOrderService(String orderNumber) throws ClientHandlerException, IllegalArgumentException, UniformInterfaceException, UriBuilderException {
         ClientConfig config = new DefaultClientConfig();
         Client client = Client.create(config);
-        WebResource webResource = client.resource(UriBuilder.fromUri(REST_URL + orderNumber).build());
+        WebResource webResource = client.resource(UriBuilder.fromUri(REST_URL_ORDER_SERVICE + orderNumber).build());
 
         try {
             ClientResponse response = webResource.type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse.class);
@@ -51,6 +52,44 @@ public class OrderClient {
         } catch (ClientHandlerException | UniformInterfaceException ex) {
             System.out.println(Arrays.toString(ex.getStackTrace()));
             return "Order-Service nicht erreichbar";
+        }
+
+        return null;
+    }
+
+    /**
+     *  Call hubba bubba service in order to decrease the number of items contained in a
+     *  slot after the Order-Service has been called successfully
+     *  @param slotNumber order number/slot number
+     *  @return error message or null if successful
+     */
+    public static String callDecreaseSlotItemCountService(String slotNumber) throws ClientHandlerException, IllegalArgumentException, UniformInterfaceException, UriBuilderException {
+        ClientConfig config = new DefaultClientConfig();
+        Client client = Client.create(config);
+        String url = String.format(REST_URL_SLOT_DECREASE_ITEM_COUNT_SERVICE, slotNumber);
+        WebResource webResource = client.resource(UriBuilder.fromUri(url).build());
+
+        try {
+            ClientResponse response = webResource.type(MediaType.APPLICATION_FORM_URLENCODED_TYPE).post(ClientResponse.class);
+            ObjectMapper mapper = new ObjectMapper();
+            String responseText = response.getEntity(String.class);
+            if (responseText.equals("OK")) {
+                System.out.println("Response: " + responseText);
+                //TODO: response seite für alles hat funktioniert
+            } else {
+                try {
+                    ErrorCode errorCode = mapper.readValue(responseText, ErrorCode.class);
+                    System.out.println("ErrorMessage: " + errorCode.getErrorMessage());
+                    return String.format("SlotDecreaseItemCount-Service %s", errorCode.getErrorMessage());
+                } catch (IOException ex) {
+                    System.out.println("Error on errorcode handling");
+                    ex.printStackTrace();
+                    return "Unbekannter Fehler beim Aufruf von SlotDecreaseItemCount-Service (siehe stdout)";
+                }
+            }
+        } catch (ClientHandlerException | UniformInterfaceException ex) {
+            System.out.println(Arrays.toString(ex.getStackTrace()));
+            return "SlotDecreaseItemCount-Service nicht erreichbar";
         }
 
         return null;
